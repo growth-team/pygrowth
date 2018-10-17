a6ec71e86ec8e138b5f7731a6ce43ebf9a036531
Takayuki Yuasa <yuasatakayuki@gmail.com>
2018-10-17 21:55:36 +0800

    eventfile.py - test plot()

--- pygrowth/cli/eventfile.py
+++ pygrowth/cli/eventfile.py
@@ -29,19 +29,18 @@ def extract(file_path):
     eventfile = pygrowth.common.eventfile.open(file_path)
     extractor = pygrowth.common.counthistory.CountHistoryExtractor()
     options = {
-        "time_axis": "absolute",
+        "time_axis": "relative",
         "time_origin": 1514734663.0,
         "channel": 0,
         "energy_range_kev": [500, 3000],
         "duration_before_origin_sec": 30.0,
         "duration_after_origin_sec": 120.0,
     }
-    counthistory = extractor.extract(eventfile, 10, options)
-    click.echo(counthistory)
-    for time_bin_center, time_bin_width, rate, rate_error in zip(
-            counthistory.time_bin_center(), counthistory.time_bin_width(),
-            counthistory.count_rate(), counthistory.count_rate_error()):
-        click.echo([time_bin_center, time_bin_width, rate, rate_error])
+    counthistory = extractor.extract(eventfile, 2, options)
+    import matplotlib.pyplot as plt
+    fig, panel = plt.subplots(1, 1, sharex=True)
+    counthistory.plot(panel)
+    fig.savefig("counthistory.png", dpi=200, bbox_inches='tight', transparent=True)
 
 
 def main():
